4285d60306b2429ba6d901eff5028b39b9f16f09
Taha Tuna <tahatuna777@gmail.com>
2023-05-21 23:11:34 +0200

    Initial Commit

---
+++ SimpleBudget/SimpleBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBudgetApp.swift
+//  SimpleBudget
+//
+//  Created by Taha Tuna on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
